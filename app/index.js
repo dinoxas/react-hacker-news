0286889b57ea93e1811eb3331d88ca1fee159573
dinoxas <dhassiotis07@gmail.com>
2019-10-25 18:38:07 +0100

    project setup

---
+++ app/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Hacker News</h1>
+    </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
