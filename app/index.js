a09434ff1415fef96ac22a33b32490db6da36a80
dinoxas <dhassiotis07@gmail.com>
2019-10-27 20:24:54 +0000

    Added theme context and Nav component

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import { ThemeProvider } from './contexts/theme'
 import Nav from './components/Nav'
 import Top from './components/Top'
 import New from './components/New'
@@ -8,24 +10,32 @@ import './index.css'
 
 
 class App extends React.Component {
+  state = {
+    theme: 'light',
+    toggleTheme: () => {
+      this.setState(({ theme }) => ({
+        theme: theme === 'light' ? 'dark' : 'light'
+      }))
+     
+    }
+  }
 
   render() {
+
     return (
       <Router>
+          <ThemeProvider value={this.state}>
+            <div className={this.state.theme}>
+              <div className='container'>
+                <Nav />
+                  <Switch>
+                    <Route exact path='/' component={Top} />
+                    <Route exact path='/new' component={New} />
+                  </Switch>
 
-          <div>
-            <div className='container'>
-              <Nav />
-   
-
-                <Switch>
-                  <Route exact path='/' component={Top} />
-                  <Route exact path='/new' component={New} />
-                </Switch>
-
+              </div>
             </div>
-          </div>
-
+          </ThemeProvider>
       </Router>
     )
   }
