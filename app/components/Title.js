9e080560767364b8130da8e7d9e9da54a6181a86
dinoxas <dhassiotis07@gmail.com>
2019-10-28 16:24:08 +0000

    Added React icon onto Title component

--- app/components/Title.js
+++ app/components/Title.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import { FaRegClone } from 'react-icons/fa';
 
 export default function Title ({ url, title, id }) {
   return url
-    ? <a className='link' href={url} target='_blank' rel='nofollow'>{title}</a>
+    ? <a className='link' href={url} target='_blank' rel='nofollow'>{title} <FaRegClone size='20' color='rgb(187, 46, 31)' /></a>
     : <Link className='link' to={`/post?id=${id}`}>{title}</Link>
 }
 
